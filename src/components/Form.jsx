26de4f51554b806ddb46fce7d298343cb7f397a0
AgostinoCaruso <agostinocaruso321@gmail.com>
2025-01-07 16:09:48 +0100

    add funzioni per il form multiplo

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,12 +1,53 @@
-function Form() {
+import { useState } from "react";
+
+function Form({ addCard }) {
+
+    const [formData, setFormData] = useState({
+        id: 0,
+        title: "",
+        content: "",
+        available: false,
+    });
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        const card = {
+            id: Math.random(),
+            title: formData.title,
+            content: formData.content,
+            available: formData.available
+        };
+
+        addCard(card);
+        setFormData({
+            id: 0,
+            title: "",
+            content: "",
+            available: false,
+        });
+    }
+
+    const handleInputChange = (e) => {
+        const { name, value, checked, type } = e.target;
+        let inputValue = type == "checkbox" ? checked : value;
+        if (inputValue == "") {
+            inputValue = "empty";
+        }
+
+        setFormData({...formData, [name]: inputValue});
+    }
+
+
     return (
         <>
-            <form >
+            <form onSubmit={handleSubmit} >
                 <input
                     type="text"
-                    name="search"
-                    placeholder="Search..."
-
+                    name="title"
+                    placeholder="Title..."
+                    value={formData.title}
+                    onChange={handleInputChange}
                 />
 
                 <button type="submit" className="">Invia</button>
